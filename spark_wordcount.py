706353a0935afd37f1395914f52e16a389660bac
kendal-win <77511524+kendal-win@users.noreply.github.com>
2026-09-24 16:45:46 -0700

    Update spark_wordcount.py

--- spark_wordcount.py
+++ spark_wordcount.py
@@ -1,8 +1,7 @@
 from pyspark import SparkContext
-
 sc = SparkContext("local", "WordCount")
-test = sc.textFile("/gutenberg/*.txt")
+text = sc.textFile("/gutenberg/*.txt")
 counts = text.flatMap(lambda line: line.split()) \
-    .map(lambda word: (word, 1)) \
-    .reduceByKey(lambda a, b: a + b)
+.map(lambda word: (word, 1)) \
+.reduceByKey(lambda a, b: a + b)
 counts.saveAsTextFile("/output_spark_wordcount")
