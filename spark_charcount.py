c8b58864c960766e63463a6f2cf9bc11f43c8312
kendal-win <77511524+kendal-win@users.noreply.github.com>
2026-09-24 17:06:11 -0700

    Update spark_charcount.py

--- spark_charcount.py
+++ spark_charcount.py
@@ -1,6 +1,7 @@
 from pyspark import SparkContext
-
 sc = SparkContext("local", "CharCount")
-text = sc.textFile ("/gutenberg/*.txt")
-chars = text.flatMap(lambda line: list(line)) \ .map (lambda ch: (ch, 1)) \ .reduceByKey(lambda a, b: a + b)
+text = sc.textFile("/gutenberg/*.txt")
+chars = text.flatMap(lambda line: list(line)) \
+.map(lambda ch: (ch, 1)) \
+.reduceByKey(lambda a, b: a + b)
 chars.saveAsTextFile("/output_spark_charcount")
