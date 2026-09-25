22cf01313317fcf817dd22453c928b5d56f88be9
kendal-win <77511524+kendal-win@users.noreply.github.com>
2026-09-24 17:06:30 -0700

    Update spark_minmax.py

--- spark_minmax.py
+++ spark_minmax.py
@@ -1,11 +1,10 @@
 from pyspark import SparkContext
-
 sc = SparkContext("local", "MinMax")
 text = sc.textFile("/gutenberg/*.txt")
-counts = text.flatMap(lambda line: line.split()) \ .map(lambda word: (word, 1)) \  .reduceByKey(lambda a, b: a + b)
-
+counts = text.flatMap(lambda line: line.split()) \
+.map(lambda word: (word, 1)) \
+.reduceByKey(lambda a, b: a + b)
 min_word = counts.min(key=lambda x: x[1])
 max_word = counts.max(key=lambda x: x[1])
-
 print("MIN:", min_word)
 print("MAX:", max_word)
